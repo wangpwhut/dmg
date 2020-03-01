7e0795558dece2242ec5c7d7b8293556f0822963
wangpwhut <51317606+wangpwhut@users.noreply.github.com>
2020-03-01 15:33:08 +0800

    Add files via upload

---
+++ dmg-data-opt/src/main/java/service/impl/InterfaceLimitServiceImpl.java
@@ -0,0 +1,19 @@
+package service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import service.InterfaceLimitService;
+
+
+
+@Service
+public class InterfaceLimitServiceImpl implements InterfaceLimitService {
+	
+
+	@Override
+	public void listEntity(Integer id) {
+		
+	}
+
+}
